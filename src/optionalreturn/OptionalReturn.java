890219a4ce2ad26d7b82e165f8106a8abd95d9f0
padovese <bhspadoveze@gmail.com>
2019-09-29 16:19:40 -0300

    feat: another examples of optional

--- src/optionalreturn/OptionalReturn.java
+++ src/optionalreturn/OptionalReturn.java
@@ -16,9 +16,38 @@ public class OptionalReturn {
     return Optional.of((double) sum / scores.length);
   }
 
+  public static double avgInDouble(Optional opt) {
+    if (opt.isPresent()) {
+      return (double) opt.get();
+    }
+    return 0.0;
+  }
+
+  public static void withLambda() {
+    Optional<Double> opt = avg(90, 100);
+    opt.ifPresent(System.out::println);
+  }
+
   public static void main(String[] args) {
-    System.out.println(avg());
+    System.out.println(
+        avg()); // if i do avg().get() i take a java.util.NoSuchElementException: No value
+    // present
     System.out.println(avg(1));
     System.out.println(avg(1, 2, 3));
+
+    System.out.println(avg().isPresent());
+    System.out.println(avg(1).isPresent());
+    System.out.println(avg(1, 2, 3).isPresent());
+
+    System.out.println(avgInDouble(avg()));
+    System.out.println(avgInDouble(avg(1)));
+    System.out.println(avgInDouble(avg(1, 2, 3)));
+
+    withLambda();
+
+    Optional<Double> opt = avg();
+    System.out.println(opt.orElse(Double.NaN));
+    System.out.println(opt.orElseGet(() -> Math.random()));
+    System.out.println(opt.orElseThrow(() -> new IllegalStateException()));
   }
 }
